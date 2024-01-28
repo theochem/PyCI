638fac20f9c2236b8e0c5220ba8df20f7a39d7c6
Marco Martínez González <mmg870630@gmail.com>
2024-01-28 02:52:08 -0500

    Reduce variable scope "overlap.cpp"

--- pyci/src/overlap.cpp
+++ pyci/src/overlap.cpp
@@ -23,8 +23,7 @@ template<class WfnType>
 double compute_overlap_thread(const WfnType &wfn1, const WfnType &wfn2, const double *coeffs1,
                               const double *coeffs2, const long start, const long end) {
     double olp = 0.0;
-    long j;
-    for (long i = start; i < end; ++i) {
+    for (long i = start, j; i < end; ++i) {
         j = wfn2.index_det(wfn1.det_ptr(i));
         if (j != -1)
             olp += coeffs1[i] * coeffs2[j];
@@ -39,11 +38,11 @@ double compute_overlap(const WfnType &wfn1, const WfnType &wfn2, const double *c
                        const double *coeffs2) {
     if (wfn1.ndet > wfn2.ndet)
         return compute_overlap<WfnType>(wfn2, wfn1, coeffs2, coeffs1);
-    long nthread = get_num_threads(), start, end;
+    long nthread = get_num_threads();
     long chunksize = wfn1.ndet / nthread + ((wfn1.ndet % nthread) ? 1 : 0);
     Vector<std::future<double>> v_threads;
     v_threads.reserve(nthread);
-    for (long i = 0; i < nthread; ++i) {
+    for (long i = 0, start, end; i < nthread; ++i) {
         start = i * chunksize;
         end = (start + chunksize < wfn1.ndet) ? start + chunksize : wfn1.ndet;
         v_threads.push_back(std::async(std::launch::async, &compute_overlap_thread<WfnType>,
