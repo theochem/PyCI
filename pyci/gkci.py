a7b15c6d834f8b21f1bda3f0af255d35ca16f2f7
Natalie <62342353+manyasha-n-m@users.noreply.github.com>
2024-04-09 15:06:18 +0000

    added q_max to add_gkci + used odometers from utility.py

--- pyci/gkci.py
+++ pyci/gkci.py
@@ -69,11 +69,14 @@ def add_gkci(wfn, t=-0.5, p=1.0, mode="cntsp", dim=3, energies=None, width=None)
     else:
         nodes = np.asarray(mode)
 
+    # Compute cost of the most important neglected determinant
+    q_max = (np.sum(nodes[: wfn.nocc_up - 1]) + (t + 1) * nodes[-1]) * p
+
     # Run odometer algorithm
     if isinstance(wfn, (pyci.doci_wfn, pyci.genci_wfn)):
-        odometer_one_spin(wfn, nodes, t, p)
+        pyci.odometer_one_spin(wfn, nodes=nodes, q_max=q_max, t=t, p=p)
     elif isinstance(wfn, pyci.fullci_wfn):
-        odometer_two_spin(wfn, nodes, t, p)
+        pyci.odometer_two_spin(wfn, nodes=nodes, q_max=q_max, t=t, p=p)
     else:
         raise TypeError(f"invalid `wfn` type `{type(wfn)}`; must be `pyci.wavefunction`")
 
@@ -213,48 +216,47 @@ def compute_nodes_interval(nbasis, es, width):
     return nodes
 
 
-def odometer_one_spin(wfn, nodes, t, p):
-    r"""Run the odometer algorithm for a one-spin wave function."""
-    old = np.arange(wfn.nocc_up, dtype=pyci.c_long)
-    new = np.copy(old)
-    # Index of last particle
-    j = wfn.nocc_up - 1
-    # Compute cost of the most important neglected determinant
-    q_max = (np.sum(nodes[: wfn.nocc_up - 1]) + (t + 1) * nodes[-1]) * p
-    # Select determinants
-    while True:
-        if new[-1] < wfn.nbasis and (np.sum(nodes[new]) + t * nodes[new[-1]]) < q_max:
-            # Accept determinant and go back to last particle
-            wfn.add_occs(new)
-            j = wfn.nocc_up - 1
-        else:
-            # Reject determinant and cycle j
-            new[:] = old
-            j -= 1
-        # Check termination condition
-        if j < 0:
-            break
-        # Generate next determinant
-        old[:] = new
-        new[j:] = np.arange(new[j] + 1, new[j] + wfn.nocc_up - j + 1)
+# def odometer_one_spin(wfn, nodes, t, p):
+#     r"""Run the odometer algorithm for a one-spin wave function."""
+#     old = np.arange(wfn.nocc_up, dtype=pyci.c_long)
+#     new = np.copy(old)
+#     # Index of last particle
+#     j = wfn.nocc_up - 1
+    
+#     # Select determinants
+#     while True:
+#         if new[-1] < wfn.nbasis and (np.sum(nodes[new]) + t * nodes[new[-1]]) < q_max:
+#             # Accept determinant and go back to last particle
+#             wfn.add_occs(new)
+#             j = wfn.nocc_up - 1
+#         else:
+#             # Reject determinant and cycle j
+#             new[:] = old
+#             j -= 1
+#         # Check termination condition
+#         if j < 0:
+#             break
+#         # Generate next determinant
+#         old[:] = new
+#         new[j:] = np.arange(new[j] + 1, new[j] + wfn.nocc_up - j + 1)
 
 
-def odometer_two_spin(wfn, nodes, t, p):
-    r"""Run the odometer algorithm for a two-spin wave function."""
-    wfn_up = pyci.doci_wfn(wfn.nbasis, wfn.nocc_up, wfn.nocc_up)
-    odometer_one_spin(wfn_up, nodes, t, p)
-    if not len(wfn_up):
-        return
-    if wfn.nocc_dn:
-        wfn_dn = pyci.doci_wfn(wfn.nbasis, wfn.nocc_dn, wfn.nocc_dn)
-        odometer_one_spin(wfn_dn, nodes, t, p)
-        if not len(wfn_dn):
-            return
-        for i in range(len(wfn_up)):
-            det_up = wfn_up[i]
-            for j in range(len(wfn_dn)):
-                wfn.add_det(np.vstack((det_up, wfn_dn[j])))
-    else:
-        det_dn = np.zeros_like(wfn_up[0])
-        for i in range(len(wfn_up)):
-            wfn.add_det(np.vstack((wfn_up[i], det_dn)))
+# def odometer_two_spin(wfn, nodes, t, p):
+#     r"""Run the odometer algorithm for a two-spin wave function."""
+#     wfn_up = pyci.doci_wfn(wfn.nbasis, wfn.nocc_up, wfn.nocc_up)
+#     odometer_one_spin(wfn_up, nodes, t, p)
+#     if not len(wfn_up):
+#         return
+#     if wfn.nocc_dn:
+#         wfn_dn = pyci.doci_wfn(wfn.nbasis, wfn.nocc_dn, wfn.nocc_dn)
+#         odometer_one_spin(wfn_dn, nodes, t, p)
+#         if not len(wfn_dn):
+#             return
+#         for i in range(len(wfn_up)):
+#             det_up = wfn_up[i]
+#             for j in range(len(wfn_dn)):
+#                 wfn.add_det(np.vstack((det_up, wfn_dn[j])))
+#     else:
+#         det_dn = np.zeros_like(wfn_up[0])
+#         for i in range(len(wfn_up)):
+#             wfn.add_det(np.vstack((wfn_up[i], det_dn)))
