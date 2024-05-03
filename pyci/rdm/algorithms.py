cf25e01bdaae9c609b0d7fc10bee82cfe856b9eb
Daniel Calero <110789386+DanielCalero1@users.noreply.github.com>
2024-05-03 23:15:30 +0000

    Adding documentation, fixing data types, adding return to optimize methods and fixing other minor problems

--- pyci/rdm/algorithms.py
+++ pyci/rdm/algorithms.py
@@ -1,39 +1,65 @@
 import numpy as np 
-from abc import ABCMeta, abstractmethod
-class Projection(metaclass=ABCMeta):
+from abc import ABC, abstractmethod
+class Projection(ABC):
     
-    @abstractmethod
-    def __init__(self, initial_guess,constraints):
+    def __init__(self, initial_guess: np.ndarray,constraints: list)-> None:
+        r"""
+        Initialize the Projection object.
+
+        Parameters
+        ----------
+        initial guess: np.ndarray
+            Initial with guess density matrix \Gamma_0.
+        constraints: list
+            List of projection operators $J$ onto convex subspaces C_i related to the constraints.
+        """
         self.initial_guess = initial_guess
         self.constraints = constraints
     
     @abstractmethod 
     def optimize(self):
+        r"""
+        Algorithm to find the optimal wave function that satisfies all the constraints. This wave function Gamma is the
+        one that minimizes ||Gamma-Gamma_0|| in the convex set C, which is the set of (approximately) N-representable density
+        matrices      
+        
+        Parameters
+        ----------
+        Specific parameters depend on the particular algorithm
+        """
         pass
 
-class Dykstra(metaclass=ABCMeta):
-    def __init__(self, initial_guess:float, constraints:list, alpha:int =1, max_iterations:int =100, eps:int =1e-6):
-        """
-        Dykstra's algorithm for projection onto convex sets.
+class Dykstra(Projection):
+    def __init__(self, initial_guess:np.ndarray, constraints:list, alpha:float =1.0, max_iterations:int =100, eps:float =1e-6)-> None:
+        r"""
+        Dykstra's class for projection onto convex sets.
 
         Parameters
         ----------
-        initial_guess : float
-            Initializing with guessed density matrix \Gamma_0.
+        initial_guess : np.ndarray
+            Initial  guess for the density matrix \Gamma_0.
         constraints : list
-            A list of projection operators onto all $J$ of the constraints as a single set, $\{\mathcal{P}_j\}_{j=0}^{J-1}$.
-        alpha : int, optional
+            List of projection operators $J$ onto convex subspaces C_i related to the constraints.
+        alpha : float
             Tunning parameter, by default 1
-        max_iterations : int, optional
+        max_iterations : int
             Number of maximum iterations, by default 100
-        eps : int, optional
+        eps : float, optional
             Tolerance, by default 1e-6
         """        
         super().__init__(initial_guess, constraints)
         self.alpha = alpha
         self.max_iterations = max_iterations
         self.eps = eps
-    def optimize(self):
+    def optimize(self)-> np.ndarray:
+        r"""
+        Dykstra's algorithm to find the optimal wave function that satisfies all the constraints        
+        
+        Returns
+        ----------
+        D: np.ndarray
+            Optimal rdm that satisfies the given constraints
+        """
         X = [np.zeros(self.initial_guess) for i in range(len(self.constraints))]
         D = np.copy(self.initial_guess)
         norm = []
@@ -46,20 +72,43 @@ class Dykstra(metaclass=ABCMeta):
             is_stop = self.alpha * abs(norm[i] - norm[i - 1]) + (1 - self.alpha) * norm[i] < self.eps
             if is_stop:
                 break
-class Neumann(metaclass=ABCMeta):
-    def __init__(self, initial_guess, constraints, alpha, max_iterations=100, eps=1e-6):
-        super().__init__(initial_guess, constraints)
-        self.alpha = alpha
-        self.max_iterations = max_iterations
-        self.eps = eps
-    def optimize(self):
+        return D
+class Neumann(Dykstra):
+    def __init__(self, initial_guess:np.ndarray, constraints:list, alpha:float =1.0, max_iterations:int =100, eps:float =1e-6)-> None:
+        super().__init__(initial_guess, constraints,alpha,max_iterations,eps)
+        r"""
+        Neumann's class for projection onto convex sets.
+
+        Parameters
+        ----------
+        initial_guess : np.ndarray
+            Initial guess for the density matrix \Gamma_0.
+        constraints : list
+            List of projection operators $J$ onto convex subspaces C_i related to the constraints.
+        alpha : float
+            Tunning parameter, by default 1
+        max_iterations : int, optional
+            Number of maximum iterations, by default 100
+        eps : float
+            Tolerance, by default 1e-6
+        """        
+    def optimize(self)-> np.ndarray:
+        r"""
+        Neumann's algorithm to find the optimal wave function that satisfies all the constraints        
+        
+        Returns
+        ----------
+        D: np.ndarray
+            Optminal rdm that satisfies the given constraints
+        """
         D = np.copy(self.initial_guess)
         norm = []
         for i in range (self.max_iterations):
-            for projection in enumerate(self.constraints):
+            for projection in self.constraints:
                 D = projection(D)
             norm.append(np.linalg.norm(D - self.initial_guess))
             is_stop = self.alpha * abs(norm[i] - norm[i - 1]) + (1 - self.alpha) * norm[i] < self.eps
             if is_stop:
                 break
+        return D
 
