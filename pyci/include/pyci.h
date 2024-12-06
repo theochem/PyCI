34ac07ce4c552485628b1ff682bccc80e76e285e
Daniel Calero <110789386+DanielCalero1@users.noreply.github.com>
2024-12-06 16:03:41 +0000

    updating pyci.h to include compute_rdms_34 function

--- pyci/include/pyci.h
+++ pyci/include/pyci.h
@@ -219,7 +219,9 @@ void setbit_det(const long, ulong *);
 
 void clearbit_det(const long, ulong *);
 
-void compute_rdms(const DOCIWfn &, const double *, double *, double *, double *, double *);
+void compute_rdms(const DOCIWfn &, const double *, double *, double *);
+
+void compute_rdms_34(const DOCIWfn &, const double *, double *, double *, double *, double *);
 
 void compute_rdms(const FullCIWfn &, const double *, double *, double *);
 
@@ -251,6 +253,8 @@ long py_popcnt(const Array<ulong>);
 long py_ctz(const Array<ulong>);
 
 pybind11::tuple py_compute_rdms_doci(const DOCIWfn &, const Array<double>);
+
+pybind11::tuple py_compute_rdms_34_doci(const DOCIWfn &, const Array<double>);
 
 pybind11::tuple py_compute_rdms_fullci(const FullCIWfn &, const Array<double>);
 
