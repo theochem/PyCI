d59492df19ed4ee5f4a5f98f5c083a0cf1fb3fc7
Marco Martínez González <mmg870630@gmail.com>
2025-02-05 10:58:18 -0500

    Add support for musllinux

--- tools/wheels/cibw_before_build_linux.sh
+++ tools/wheels/cibw_before_build_linux.sh
@@ -1,34 +1,18 @@
 set -xe
 
 
-# # NIGHTLY_FLAG=""
+if grep -q "manylinux" /etc/os-release 2>/dev/null; then
+    echo "Inside a manylinux container"
+    yum install -y git make
+elif ldd --version 2>&1 | grep -q "musl"; then
+    echo "Inside a musllinux container"
+    apk add git make
+else
+    echo "Unknown or non-standard Linux environment"
+    exit 1
+fi
 
-# # if [ "$#" -eq 1 ]; then
-# #     PROJECT_DIR="$1"
-# # elif [ "$#" -eq 2 ] && [ "$1" = "--nightly" ]; then
-# #     NIGHTLY_FLAG="--nightly"
-# #     PROJECT_DIR="$2"
-# # else
-# #     echo "Usage: $0 [--nightly] <project_dir>"
-# #     exit 1
-# # fi
 
-# printenv
-# # Update license
-# cat $PROJECT_DIR/tools/wheels/LICENSE_linux.txt >> $PROJECT_DIR/LICENSE.txt
-
-# TODO: delete along with enabling build isolation by unsetting
-# CIBW_BUILD_FRONTEND when scipy is buildable under free-threaded
-# python with a released version of cython
-# FREE_THREADED_BUILD="$(python -c"import sysconfig; print(bool(sysconfig.get_config_var('Py_GIL_DISABLED')))")"
-# if [[ $FREE_THREADED_BUILD == "True" ]]; then
-#     python -m pip install -U --pre pip
-#     python -m pip install -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple numpy cython
-#     python -m pip install git+https://github.com/serge-sans-paille/pythran
-#     python -m pip install ninja meson-python pybind11
-# fi
-
-yum install -y git make
 
 # download dependencies
 # echo $PROJECT_DIR/deps
